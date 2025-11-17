8fc87d9dd8cefade0d53384be5e5d9d856cee9f5
Amit <amitdubal2005@gmail.com>
2025-11-17 20:16:02 +0530

    path align

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
             </div>
             <div className="project-container">
                 <a href="https://www.msrealtyproject.in/" target='_blank'><img src="./msrealty.jpeg" alt="" /></a>
-                <a href="https://github.com/amitdubal09/SkillScan-AI.git" target='_blank'><img src="./SkillScan.jpeg" alt="SkillScan" /></a>
+                <a href="https://github.com/amitdubal09/SkillScan-AI.git" target='_blank'><img src="./skillScan.jpeg" alt="SkillScan" /></a>
                 <a href="https://github.com/amitdubal09/online-auction-system.git" target='_blank'><img src="./auction.jpeg" alt="online auction system" /></a>
                 <a href="https://github.com/amitdubal09/weather-web-application.git" target='_blank'><img src="./weather.jpeg" alt="Weather web-application" /></a>
                 <a href="https://github.com/amitdubal09/tic-tac-toe.git" target='_blank'><img src="./tic-tac-toe.jpeg" alt="tic-tac-toe" /></a>
