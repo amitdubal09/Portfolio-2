0b5d1e7e18b12b3f3eecf96a82d7106304da47ad
Amit <amitdubal2005@gmail.com>
2025-11-17 21:35:36 +0530

    css updated

--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -10,8 +10,8 @@ const Hero = () => {
           <h1 className='color'>I'm Amit Dubal,<br></br> <span> Web Developer Based in India.</span></h1>
           <p>I am a fresher web developer skilled in HTML, CSS, JavaScript, MySQL, PHP, and ReactJS. I enjoy building responsive websites and am eager to learn and grow in real projects.</p>
           <div className="buttons">
-            <div className="first"><a href="#contact">Connect With Me</a></div>
-            <div className="second"><a href="https://drive.google.com/file/d/1bkmRwoRQ1bRUIYtKoVy28t1gflm9wpjl/view?usp=drive_link" target='_blank'>My Resume</a></div>
+            <div><a className="first click" href="#contact">Connect With Me</a></div>
+            <div><a className="click second" href="https://drive.google.com/file/d/1bkmRwoRQ1bRUIYtKoVy28t1gflm9wpjl/view?usp=drive_link" target='_blank'>My Resume</a></div>
           </div>
         </div>
       </div>
